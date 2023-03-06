b75b9a967e17a0c9576e4f08fcdbe78b631a6e8e
Louisa <louisa.whitaker@gmail.com>
2023-03-06 02:13:40 +0000

    typescript-react hot button

---
+++ typescript-react-hot-button/my-app/src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import './App.css';
+import HotButton from './HotButton';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <HotButton className="btnClass" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ typescript-react-hot-button/my-app/src/HotButton.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+
+interface HotBtnProps {
+  className: string;
+}
+
+interface HotBtnState {
+  clicks: number;
+}
+
+class HotBtn extends React.Component<HotBtnProps, HotBtnState> {
+  constructor(props: HotBtnProps) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+    this.state = { clicks: 0 };
+  }
+
+  handleClick() {
+    const updateCount = this.state.clicks + 1;
+    this.setState({ clicks: updateCount });
+  }
+
+  render() {
+    let btnClass = 'btn-blue';
+
+    if (this.state.clicks >= 3 && this.state.clicks < 6) {
+      btnClass = 'btn-purple';
+    } else if (this.state.clicks >= 6 && this.state.clicks < 12) {
+      btnClass = 'btn-pink';
+    } else if (this.state.clicks >= 12 && this.state.clicks < 15) {
+      btnClass = 'btn-orange';
+    } else if (this.state.clicks >= 15 && this.state.clicks < 18) {
+      btnClass = 'btn-yellow';
+    } else if (this.state.clicks >= 18 && this.state.clicks < 21) {
+      btnClass = 'btn-white';
+    }
+    return (
+      <button className={btnClass} onClick={this.handleClick}>
+        Hot Button
+      </button>
+    );
+  }
+}
+
+export default HotBtn;
